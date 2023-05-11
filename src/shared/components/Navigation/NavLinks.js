04c619491993da74c5407204ceb9eff28d59cbd5
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-11 11:50:32 -0300

    Adding Navlinks

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+const NavLinks = props => {
+    return (
+        <ul className="nav-links">
+            <li>
+                <NavLink to="/" exact>
+                    ALL USERS
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/u1/places">MY PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/places/new">ADD PLACE</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
+            </li>
+        </ul>
+    );
+};
+export default NavLinks;
+
+// The difference between the Link and NavLink components is that NavLink allows you to add styling to the active link. It also adds a CSS class of active to the active link.
