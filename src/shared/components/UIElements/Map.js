a42b11d1dc41a1dba54a4bd5fc8f743761913ce1
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-12 15:37:15 -0300

    Adding a Custom maker on using OpenLayers

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -8,12 +8,36 @@ const Map = props => {
     const { center, zoom } = props;
 
     useEffect(() => {
+        let marker = new window.ol.Feature({
+            geometry: new window.ol.geom.Point(window.ol.proj.fromLonLat([center.lng, center.lat]))
+        });
+
+        let markerStyle = new window.ol.style.Style({
+            image: new window.ol.style.Circle({
+                radius: 10,
+                fill: new window.ol.style.Fill({ color: '#fff' }),
+                stroke: new window.ol.style.Stroke({
+                    color: [255, 56, 96],
+                    width: 5
+                })
+            })
+        });
+
+        marker.setStyle(markerStyle);
+
+        const layer = new window.ol.layer.Vector({
+            source: new window.ol.source.Vector({
+                features: [marker]
+            })
+        });
+
         new window.ol.Map({
             target: mapRef.current.id,
             layers: [
                 new window.ol.layer.Tile({
                     source: new window.ol.source.OSM()
-                })
+                }),
+                layer
             ],
             view: new window.ol.View({
                 center: window.ol.proj.fromLonLat([center.lng, center.lat]),
