9dae002c8006439c94561ef20349ae0df45f1d52
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-15 15:05:13 -0300

    Share Input Values and Add Multiple Inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
+// useCallback is used to avoid infinite loop. The useCallback will only be recreated if the dependencies change. If the function is reused (id, value, isValid), the function will not be recreated.
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+    const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
     return (
         <form className="place-form">
-            <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title"
+                onInput={titleInputHandler}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid description (at least 5 characters)"
+                onInput={descriptionInputHandler}
+            />
         </form>
     );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -24,6 +24,13 @@ const inputReducer = (state, action) => {
 
 const Input = newPlace => {
     const [inputState, dispatch] = useReducer(inputReducer, { value: '', isTouched: false, isValid: false });
+
+    const { id, onInput } = newPlace;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
 
     const changeHandler = event => {
         dispatch({ type: 'CHANGE', val: event.target.value, validators: newPlace.validators });
