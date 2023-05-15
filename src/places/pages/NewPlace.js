6de1083335ff51a49d3ad0ce4dc970f56ef90e69
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-15 15:42:09 -0300

    Managing Form-wide State

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,14 +1,58 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 // useCallback is used to avoid infinite loop. The useCallback will only be recreated if the dependencies change. If the function is reused (id, value, isValid), the function will not be recreated.
-const NewPlace = () => {
-    const titleInputHandler = useCallback((id, value, isValid) => {}, []);
 
-    const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+// The reducer logic
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                // With this logic if we have one false, the formIsValid will be false.
+                if (inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid;
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    // for example if we dispatch a new title, the title will be updated and the description will not be updated.
+                    [action.inputId]: { value: action.value, isValid: action.isValid }
+                },
+                isValid: formIsValid
+            };
+        default:
+            return state;
+    }
+};
+
+const NewPlace = () => {
+    // Initial state
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id });
+    }, []);
 
     return (
         <form className="place-form">
@@ -19,7 +63,7 @@ const NewPlace = () => {
                 label="Title"
                 validators={[VALIDATOR_REQUIRE()]}
                 errorText="Please enter a valid title"
-                onInput={titleInputHandler}
+                onInput={inputHandler}
             />
             <Input
                 id="description"
@@ -27,8 +71,11 @@ const NewPlace = () => {
                 label="Description"
                 validators={[VALIDATOR_MINLENGTH(5)]}
                 errorText="Please enter a valid description (at least 5 characters)"
-                onInput={descriptionInputHandler}
+                onInput={inputHandler}
             />
+            <Button type="submit" disabled={!formState.isValid}>
+                ADD PLACE
+            </Button>
         </form>
     );
 };
