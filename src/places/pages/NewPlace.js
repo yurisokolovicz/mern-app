4a89499fcd250517d637ee1155349f670e539c19
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-17 11:39:44 -0300

    A Reducer Custom Hook and using it in NewPlace.js

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,43 +1,66 @@
-import React, { useCallback, useReducer } from 'react';
+import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook'; // Custom hook with reducer logic
 import './PlaceForm.css';
 
 // useCallback is used to avoid infinite loop. The useCallback will only be recreated if the dependencies change. If the function is reused (id, value, isValid), the function will not be recreated.
 
-// The reducer logic
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'INPUT_CHANGE':
-            let formIsValid = true;
-            for (const inputId in state.inputs) {
-                // With this logic if we have one false, the formIsValid will be false.
-                if (inputId === action.inputId) {
-                    formIsValid = formIsValid && action.isValid;
-                } else {
-                    formIsValid = formIsValid && state.inputs[inputId].isValid;
-                }
-            }
-            return {
-                ...state,
-                inputs: {
-                    ...state.inputs,
-                    // for example if we dispatch a new title, the title will be updated and the description will not be updated.
-                    [action.inputId]: { value: action.value, isValid: action.isValid }
-                },
-                isValid: formIsValid
-            };
-        default:
-            return state;
-    }
-};
+///////////////////////////////////////////////////////////////////////////////
+// The reducer logic was moved to separated component, adapted in a custom hook, because we will use it in other places.
+
+// import { useCallback, useReducer } from 'react';
+// const formReducer = (state, action) => {
+//     switch (action.type) {
+//         case 'INPUT_CHANGE':
+//             let formIsValid = true;
+//             for (const inputId in state.inputs) {
+//                 if (inputId === action.inputId) {
+//                     formIsValid = formIsValid && action.isValid;
+//                 } else {
+//                     formIsValid = formIsValid && state.inputs[inputId].isValid;
+//                 }
+//             }
+//             return {
+//                 ...state,
+//                 inputs: {
+//                     ...state.inputs,
+//                     [action.inputId]: { value: action.value, isValid: action.isValid }
+//                 },
+//                 isValid: formIsValid
+//             };
+//         default:
+//             return state;
+//     }
+// };
+
+// const NewPlace = () => {
+//     // Initial state
+//     const [formState, dispatch] = useReducer(formReducer, {
+//         inputs: { // this is passed to useForm (here) - lines below
+//             title: {
+//                 value: '',
+//                 isValid: false
+//             },
+//             description: {
+//                 value: '',
+//                 isValid: false
+//             },
+//             address: {
+//                 value: '',
+//                 isValid: false
+//             }
+//         },
+//         isValid: false
+//     });
 
 const NewPlace = () => {
-    // Initial state
-    const [formState, dispatch] = useReducer(formReducer, {
-        inputs: {
+    const [formState, inputHandler] = useForm(
+        // getting information (formState and inputHandler) from useForm (in form-hook.js)
+        {
+            // initialInputs - first argument of useForm (in form-hook.js)
             title: {
                 value: '',
                 isValid: false
@@ -51,12 +74,14 @@ const NewPlace = () => {
                 isValid: false
             }
         },
-        isValid: false
-    });
+        // initialFormValidity - second argument of useForm (in form-hook.js)
+        false
+    );
 
-    const inputHandler = useCallback((id, value, isValid) => {
-        dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id });
-    }, []);
+    ///////// transfered to form-hook.js
+    // const inputHandler = useCallback((id, value, isValid) => {
+    //     dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id });
+    // }, []);
 
     const placeSubmitHandler = event => {
         event.preventDefault();
